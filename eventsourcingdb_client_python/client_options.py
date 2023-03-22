44459456c37bfd0319fae9484b16a673d2549b3a
Alexander Kampf <alexander.kampf@thenativeweb.io>
2023-03-22 11:14:20 +0100

    Remove old files

--- eventsourcingdb_client_python/client_options.py
+++
@@ -1,6 +0,0 @@
-class ClientOptions:
-	def __init__(self, timeoutMilliseconds=None, accessToken=None, protocolVersion=None, maxTries=None):
-		self.timeoutMilliseconds = timeoutMilliseconds
-		self.accessToken = accessToken
-		self.protocolVersion = protocolVersion
-		self.maxTries = maxTries
