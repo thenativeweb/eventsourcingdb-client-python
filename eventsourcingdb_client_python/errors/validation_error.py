79732b8ad69496cc199cd7840d7d0e64c75dc1ab
Kuro <alexander.kampf@thenativeweb.io>
2023-04-13 09:18:13 +0200

    Update eventsourcingdb_client_python/errors/validation_error.py
    Co-authored-by: Golo Roden <golo.roden@thenativeweb.io>

--- eventsourcingdb_client_python/errors/validation_error.py
+++ eventsourcingdb_client_python/errors/validation_error.py
@@ -2,5 +2,5 @@ from .custom_error import CustomError
 
 
 class ValidationError(CustomError):
-	def __int__(self, cause: str):
+	def __init__(self, cause: str):
 		super().__init__(f'Validation error occurred: {cause}')
